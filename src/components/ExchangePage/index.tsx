442aabddfa28fe966f483fa36e4f65149c5e5479
Anastasia Skripnik <scripnik.a21@cf.dnu.dp.ua>
2021-11-30 14:36:12 +0200

    added block with price content

--- src/components/ExchangePage/index.tsx
+++ src/components/ExchangePage/index.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import { observer } from 'mobx-react';
 import classNames from 'classnames';
-//components
 
 // style
 import styles from './styles.module.scss';
-import TimeBar from 'components/TimeBar';
 
-const priceBlocks = [
-	{ id: 1, caption: 'Last trade price', text: '0.00167200 ETH' },
-	{ id: 2, caption: '24 hour price', text: '2.32%' },
-	{ id: 3, caption: '24 hour volume', text: '42.738493 OMG' },
-];
+//components
+import ExchangePrice from 'components/ExchangePrice'
+
+import TimeBar from 'components/TimeBar';
 
 const ExchangePage: React.FC = observer(() => {
 	return (
@@ -21,14 +18,7 @@ const ExchangePage: React.FC = observer(() => {
 				<TimeBar />
 			</div>
 
-			<div className={styles.priceContainer}>
-				{priceBlocks.map((block) => (
-					<div className={styles.priceColumn} key={block.id}>
-						<h4 className={styles.caption}>{block.caption}</h4>
-						<p className={classNames(styles.text, block.id%2===0?'green':'')}>{block.text}</p>
-					</div>
-				))}
-			</div>
+			<ExchangePrice />
 
 			<div>Chart</div>
 		</>
---
+++ src/components/ExchangePrice/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { observer } from 'mobx-react';
+import classNames from 'classnames';
+//components
+
+// style
+import styles from './styles.module.scss';
+import TimeBar from 'components/TimeBar';
+
+const priceBlocks = [
+	{ id: 1, title: 'Last trade price', text: '0.00167200 ETH' },
+	{ id: 2, title: '24 hour price', text: '2.32%' },
+	{ id: 3, title: '24 hour volume', text: '42.738493 OMG' },
+];
+
+const ExchangePrice: React.FC = observer(() => {
+	return (
+		<div className={styles.container}>
+			<div className={styles.priceListWrapper}>
+				{priceBlocks.map((block) => (
+					<div className={styles.priceItem} key={block.id}>
+						<h4 className={styles.title}>{block.title}</h4>
+						<p
+							className={classNames(
+								styles.text,
+								block.id % 2 === 0 ? styles.green : ''
+							)}
+						>
+							{block.text}
+						</p>
+					</div>
+				))}
+			</div>
+		</div>
+	);
+});
+
+export default ExchangePrice;
