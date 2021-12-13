e21efb9e929dce1a13f158c3d14b622aa0f6cc33
Anastasia Skripnik <scripnik.a21@cf.dnu.dp.ua>
2021-12-13 09:52:31 +0200

    refactoring

--- src/components/HeaderProfile/index.tsx
+++ src/components/HeaderProfile/index.tsx
@@ -7,6 +7,8 @@ import Avatar from 'components/HeaderAvatar';
 import ProfileInfo from 'components/HeaderProfileInfo';
 
 const HeaderProfile: React.FC = () => {
+
+	
 	return (
 		<div className={styles.headerProfileContainer}>
 			<ProfileInfo />
--- src/utils/chartOptions.ts
+++ src/utils/chartOptions.ts
@@ -1,49 +1,49 @@
 import dayjs from 'dayjs';
 
-export const setOptions = (type:string) => { 
-    return {
-        chart: {
-            height: 350,
-            type: type,
-        },
-        title: {
-            text: '',
-            align: 'left',
-        },
-        annotations: {
-            xaxis: [
-                {
-                    x: 'Oct 06 14:00',
-                    borderColor: '#00E396',
-                    label: {
-                        borderColor: '#00E396',
-                        style: {
-                            fontSize: '12px',
-                            color: '#fff',
-                            background: '#00E396',
-                        },
-                        orientation: 'horizontal',
-                        offsetY: 7,
-                        text: 'Annotation Test',
-                    },
-                },
-            ],
-        },
-        tooltip: {
-            enabled: true,
-        },
-        xaxis: {
-            type: 'category',
-            labels: {
-                formatter: function (val: any) {
-                    return dayjs(val).format('MMM DD HH:mm');
-                },
-            },
-        },
-        yaxis: {
-            tooltip: {
-                enabled: true,
-            },
-        },
-    };
-}
+export const setOptions = (type: string) => {
+	return {
+		chart: {
+			height: 350,
+			type: type,
+		},
+		title: {
+			text: '',
+			align: 'left',
+		},
+		annotations: {
+			xaxis: [
+				{
+					x: 'Oct 06 14:00',
+					borderColor: '#00E396',
+					label: {
+						borderColor: '#00E396',
+						style: {
+							fontSize: '12px',
+							color: '#fff',
+							background: '#00E396',
+						},
+						orientation: 'horizontal',
+						offsetY: 7,
+						text: 'Annotation Test',
+					},
+				},
+			],
+		},
+		tooltip: {
+			enabled: true,
+		},
+		xaxis: {
+			type: 'category',
+			labels: {
+				formatter: function (val: any) {
+					return dayjs(val).format('MMM DD HH:mm');
+				},
+			},
+		},
+		yaxis: {
+			tooltip: {
+				enabled: true,
+			},
+		},
+	};
+};
